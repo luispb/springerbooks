520468903dd26cb122bf060a421fe5d2b616a5b0
Luis Panzano <luispb@microsoft.com>
2020-05-02 11:31:39 +0200

    Added columns

--- download_textbooks.py
+++ download_textbooks.py
@@ -11,19 +11,19 @@ def ensure_dir(file_path):
 df = pd.read_excel("Free+English+textbooks.xlsx")
 for index, row in df.iterrows():
         # loop through the excel list
-        file_name = f"{row.loc['Book Title']}_{row.loc['Edition']}".replace('/','-').replace(':','-')
+        dir_name = f"{row.loc['Book Title']}_{row.loc['Edition']}".replace('/','-').replace(':','-')
+        file_name = f"{row.loc['Book Title']} - {row.loc['Author']} - {row.loc['Edition']} - {row.loc['Electronic ISBN']} - {row.loc['Publisher']}".replace('/','-').replace(':','-')
         url = f"{row.loc['OpenURL']}"
         r = requests.get(url) 
-        ensure_dir(f"./download/{file_name}/")
+        ensure_dir(f"./download/{dir_name}/")
         download_url = f"{r.url.replace('book','content/pdf')}.pdf"
-        wget.download(download_url, f"./download/{file_name}/{file_name}.pdf") 
-        print(f"downloading {file_name}.pdf Complete ....")
+        wget.download(download_url, f"./download/{dir_name}/{file_name}.pdf") 
+        print(f" downloading {file_name}.pdf Complete ....")
         try:
             download_url = f"{r.url.replace('book','download/epub')}.epub"
-            wget.download(download_url, f"./download/{file_name}/{file_name}.epub") 
-            print(f"downloading {file_name}.epub Complete ....")
+            wget.download(download_url, f"./download/{dir_name}/{file_name}.epub") 
+            print(f" downloading {file_name}.epub Complete ....")
         except HTTPError as error:
-            print(f"{file_name}.epub not found ....")
-
+            print(f" {file_name}.epub not found ....")
 
          
