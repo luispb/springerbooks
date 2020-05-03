d2b6b5cbd585961c1904a689d0705d038bc060d3
Luis Panzano <luispb@microsoft.com>
2020-05-03 10:54:43 +0200

    v0.1

--- download_textbooks.py
+++ download_textbooks.py
@@ -8,22 +8,26 @@ def ensure_dir(file_path):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-df = pd.read_excel("Free+English+textbooks.xlsx")
+df = pd.read_excel("C:/Git/springerbooks/Free+English+textbooks.xlsx")
 for index, row in df.iterrows():
         # loop through the excel list
-        dir_name = f"{row.loc['Book Title']}_{row.loc['Edition']}".replace('/','-').replace(':','-')
-        file_name = f"{row.loc['Book Title']} - {row.loc['Author']} - {row.loc['Edition']} - {row.loc['Electronic ISBN']} - {row.loc['Publisher']}".replace('/','-').replace(':','-')
+        book_folder_name = f"{row.loc['Book Title']}_{row.loc['Edition']}".replace('/','-').replace(':','-')
+        file_name = f"{row.loc['Book Title']} - {row.loc['Author']} - {row.loc['Electronic ISBN']} - {row.loc['Publisher']}".replace('/','-').replace(':','-')
         url = f"{row.loc['OpenURL']}"
         r = requests.get(url) 
-        ensure_dir(f"./download/{dir_name}/")
-        download_url = f"{r.url.replace('book','content/pdf')}.pdf"
-        wget.download(download_url, f"./download/{dir_name}/{file_name}.pdf") 
-        print(f" downloading {file_name}.pdf Complete ....")
+        book_download_path = f"P:/Libros/Para importar a Calibre/{book_folder_name}/"
+        ensure_dir(f"{book_download_path}")
+        try:
+            download_url = f"{r.url.replace('book','content/pdf')}.pdf"
+            wget.download(download_url, f"{book_download_path}{file_name}.pdf") 
+            print(f" downloading {file_name}.pdf Complete ....")
+        except HTTPError as error_pdf:
+            print(f" {file_name}.pdf not found ....")
         try:
             download_url = f"{r.url.replace('book','download/epub')}.epub"
-            wget.download(download_url, f"./download/{dir_name}/{file_name}.epub") 
+            wget.download(download_url, f"{book_download_path}{file_name}.epub") 
             print(f" downloading {file_name}.epub Complete ....")
-        except HTTPError as error:
+        except HTTPError as error_epub:
             print(f" {file_name}.epub not found ....")
 
          
